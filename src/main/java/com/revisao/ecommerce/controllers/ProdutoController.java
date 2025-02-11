2bd6c1fa092cc95a909bdc32b667c8eb293db00a
fiamabrenda <fiamabrenda94@gmail.com>
2025-02-11 10:58:15 -0300

    :lipstick: feat: implementação da entidade produto e seus endpoints

--- src/main/java/com/revisao/ecommerce/controllers/ProdutoController.java
+++ src/main/java/com/revisao/ecommerce/controllers/ProdutoController.java
@@ -3,19 +3,29 @@ package com.revisao.ecommerce.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revisao.ecommerce.dto.ProdutoDTO;
 import com.revisao.ecommerce.services.ProdutoService;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 @RestController
 @RequestMapping
 public class ProdutoController {
 
 	@Autowired
 	ProdutoService service;
-	
 
+	@GetMapping
+	public List<ProdutoDTO> findAll(){
+		return service.findAll();
+	}
+	
+	@GetMapping(value = "/pagina")
+	public Page<ProdutoDTO> findAll(Pageable pagina){
+		return service.findPagina(pagina);
+	}
 }
